23a1e812077ce69719f2dfe9e9b8d4c6470b53b6
Roman Kalachik <roman.kalachik@devexpress.com>
2020-12-07 13:30:10 +0300

    some small changes

--- Bases/ResourceLoader.Generated.cs
+++ Bases/ResourceLoader.Generated.cs
@@ -3,8 +3,8 @@ namespace DevExpress.Blazor.Internal {
     public abstract partial class ScriptModulesLoaderBase {
         partial void AddScriptLoaderQueryParts(StringBuilder sb) {
             sb.Append("?version=");
-            sb.Append("cjs25eb61ac.js");
-            sb.Append("esm2faa4817.js");
+            sb.Append("cjs745794f9.js");
+            sb.Append("esm4d9dccdc.js");
         }
     }
 }
